bca2eb6a2dc3acdb3341799cac4107c521965602
Krishnan S <skrish22195@gmail.com>
2023-09-20 21:57:13 +0530

    Fix response structure for GET bookmark API with search

--- app/modules/bookmark/bookmark.handler.js
+++ app/modules/bookmark/bookmark.handler.js
@@ -1,5 +1,6 @@
 const bookmarkModel = require('./bookmark.model');
 const typesence = require('../../utils/typesense');
+const { default: mongoose } = require('mongoose');
 /**
  * Save bookmark document in db
  * @param {*} req
@@ -28,30 +29,41 @@ async function createBookmark(req, res) {
  */
 async function getBookmarks(req, res) {
   const { pageNumber = 1, recordsPerPage = 10, searchText } = req.query;
-  
-  if(searchText){
+  if (searchText) {
     const typesenseClient = await typesence.getClient();
+    //TODO => Add pagination options
     const searchOptions = {
       q: searchText,
       query_by: ['content', 'title', 'url']
-    }
-    const searchResult = await typesenseClient.collections('bookmarks').documents().search(searchOptions)
+    };
+    let bookmarks = [];
+    const searchResult = await typesenseClient.collections('bookmarks').documents().search(searchOptions);
 
+    if (searchResult?.hits?.length) {
+      const matchingDocIds = searchResult.hits.map(hit => {
+        if (hit?.document?.id) {
+          return new mongoose.Types.ObjectId(hit.document.id);
+        }
+      });
+
+      if (matchingDocIds.length) {
+        bookmarks = await bookmarkModel.find({ _id: { $in: matchingDocIds } }, null, { sort: { updatedAt: -1 } });
+      }
+    }
     return res.json({
       message: 'Bookmarks fetched',
-      data: searchResult
-    })
+      data: bookmarks
+    });
+  } else {
+    const skip = (pageNumber - 1) * recordsPerPage;
+    const options = { skip, limit: Number(recordsPerPage), sort: { updatedAt: -1 } };
+    bookmarks = await bookmarkModel.find({}, null, options);
+
+    res.json({
+      message: 'Bookmarks fetched',
+      data: bookmarks
+    });
   }
-
-  const skip = (pageNumber - 1) * recordsPerPage;
-  const options = { skip, limit: Number(recordsPerPage), sort: { updatedAt: -1 } };
-  const bookmarks = await bookmarkModel.find({}, null, options);
-
-  res.json({
-    message: 'Bookmarks fetched',
-    data: bookmarks
-  })
-
 }
 
 module.exports.createBookmark = createBookmark;
