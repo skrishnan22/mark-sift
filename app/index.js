bed15bc38f43318d72c23a641a337d2567890059
Krishnan S <skrish22195@gmail.com>
2023-09-22 23:16:17 +0530

    Webapp: Add cors middleware to support request from extension

--- app/index.js
+++ app/index.js
@@ -4,11 +4,13 @@ const app = new express();
 const bookmarkRouter = require('./modules/bookmark/bookmark.route');
 const db = require('./utils/db');
 const typesense =  require('./utils/typesense');
+const cors = require('cors');
 
 (async () => {
 
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
+  app.use(cors());
   await db.connect();
   await typesense.getClient();
   app.get('/health', (req, res) => res.json({ ok: true }));
